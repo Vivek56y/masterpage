81b25cca8bce3c61cd339b0ee131f54c50f46e94
Vivek Yadav <158554751+Vivek56y@users.noreply.github.com>
2026-03-02 03:13:53 +0530

    Update Home.jsx

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -14,7 +14,7 @@ useEffect(()=>{
             setLoading(true);
             setError("");
             const token = localStorage.getItem("token");
-            const response = await fetch("http://localhost:5000/products", {
+            const response = await fetch("https://masterpage-zeta.vercel.app/products", {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
